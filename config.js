98891a9c6634838375ce82734e6087a3375ad3b3
slrtech <sam.luc.rod@gmail.com>
2025-03-09 17:46:06 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Configuration for the application
 window.API_URL = "https://app.integracao.cloud/webhook";  // Replace with your actual API URL
-window.API_TOKEN = "035e1551ff2bc4d1ef674d78e126eed4ab26b043";      // Replace with your actual API token
+window.API_TOKEN = "";      // Replace with your actual API token
